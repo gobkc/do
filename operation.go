dcc6b6a79ac2cfe3ddc6072f3956be8a97fbf535
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2024-10-28 15:42:33 +0800

    added diff function

--- operation.go
+++ operation.go
@@ -219,3 +219,37 @@ func Poller[QueryResult any](query func(q *QueryResult) error) *PollerImpl[Query
 	}()
 	return poller
 }
+
+type DiffResp[T comparable] struct {
+	Added   []T
+	Deleted []T
+}
+
+// Diff The diff function is used to analyze the items to be added and deleted between two slices.
+// example:	var news = []string{`aa1`, `aa2`, `aa3`}
+// var olds = []string{`aa1`, `aa4`}
+// diffs := Diff(olds, news)
+// result:added: aa2,aa3,  deleted:aa4
+func Diff[T comparable](olds, news []T) (resp DiffResp[T]) {
+	oldMap := make(map[T]struct{})
+	newMap := make(map[T]struct{})
+
+	for _, item := range olds {
+		oldMap[item] = struct{}{}
+	}
+
+	for _, item := range news {
+		newMap[item] = struct{}{}
+		if _, ok := oldMap[item]; !ok {
+			resp.Added = append(resp.Added, item)
+		}
+	}
+
+	for _, item := range olds {
+		if _, ok := newMap[item]; !ok {
+			resp.Deleted = append(resp.Deleted, item)
+		}
+	}
+
+	return resp
+}
